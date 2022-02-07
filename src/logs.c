57d178754223a8bc7c3500b60370883a1cac1795
matubu <78111467+matubu@users.noreply.github.com>
2022-02-07 16:42:29 +0100

    Update logs.c

--- src/logs.c
+++ src/logs.c
@@ -3,29 +3,12 @@
 #include <time.h>
 #include "utils.h"
 
-#define INFO_LOG "logs/info.log"
-#define ERRORS_LOG "logs/errors.log"
-
-void	logfile(char *filename, char *str)
+void	log(char *str)
 {
-	int		fd = open(filename, O_CREAT|O_APPEND|O_WRONLY, S_IRWXU);
 	time_t now = time(NULL);
 	struct tm *tm = localtime(&now);
 	char *date = asctime(tm);
 
-	if (fd == -1)
-		print(2, "Error writing in log file\n");
 	date[24] = '\0';
-	tprint(fd, "%s: %s\n", date, str);
-	close(fd);
-}
-
-void	loginfo(char *str)
-{
-	logfile(INFO_LOG, str);
-}
-
-void	logerror(char *str)
-{
-	logfile(ERRORS_LOG, str);
+	tprint(2, "%s: %s\n", date, str);
 }
