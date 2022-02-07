ab6cdf9d5276d1160fbab40e6e9d82f9beab451a
matubu <78111467+matubu@users.noreply.github.com>
2022-02-07 22:54:51 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <pthread.h>
 #include "web_server.h"
-#include "logs.h"
 
 #define THREADS_COUNT 1
 
@@ -9,6 +8,6 @@ int main()
 	pthread_t threads[THREADS_COUNT];
 
 	if (pthread_create(&threads[0], NULL, init_server, NULL))
-		log("Error creating thread for server");
+		tprint(2, "Error creating thread for server");
 	pthread_exit(NULL);
 }
